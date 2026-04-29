f87833707c8f9fff710ca36bcfc85c5c710f6fbe
raymond alvarez <raymond.alvarez1127@gmail.com>
2026-04-29 15:11:33 -0700

    feat: complete gallery of horns with dynamic json mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css';
 import Header from './Header';
 import Gallery from './Gallery';
 import Footer from './Footer';
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      {/* LAB01: Your App component should render a Header, Footer and Gallery component, each of which is defined in their own files. */}
       <Header />
       <Gallery />
       <Footer />
